65540851d2bc1020e1c047dd75a834d821d5e98f
Roger Lipscombe <roger@differentpla.net>
2011-02-27 19:58:48 +0000

    Add some comments, tidy up the Rx stuff.
    See #1.

--- Src/NEmplode.Tool/Program.cs
+++ Src/NEmplode.Tool/Program.cs
@@ -15,30 +15,33 @@ namespace NEmplode.Tool
             // To find an empeg, we do a UDP broadcast on port 8300 with a single question mark in it.
             const int empegDiscoveryPort = 8300;
 
-            var discovery = Observable.Defer(() => Observable.Using(() =>
-                                                                        {
-                                                                            var localEndPoint = new IPEndPoint(IPAddress.Any, empegDiscoveryPort);
-                                                                            UdpClient client = new UdpClient(localEndPoint);
-                                                                            client.EnableBroadcast = true;
-                                                                            return client;
-                                                                        },
-                                                                    client =>
-                                                                        {
-                                                                            byte[] requestBytes = new byte[] { 0x3F }; // Single '?'
-                                                                            var remoteEndPoint = new IPEndPoint(IPAddress.Broadcast, empegDiscoveryPort);
-                                                                            client.Send(requestBytes, requestBytes.Length, remoteEndPoint);
+            // Defer prevents us from creating the UdpClient and calling Send until someone actually subscribes.
+            var discovery = Observable.Defer(
+                // using (var client = new UdpClient(...)) { ... }
+                () => Observable.Using(
+                    () => new UdpClient(new IPEndPoint(IPAddress.Any, empegDiscoveryPort)) { EnableBroadcast = true },
+                    client =>
+                        {
+                            // Send out the request.
+                            byte[] requestBytes = new byte[] { 0x3F }; // Single '?'
+                            var remoteEndPoint = new IPEndPoint(IPAddress.Broadcast, empegDiscoveryPort);
+                            client.Send(requestBytes, requestBytes.Length, remoteEndPoint);
 
-                                                                            IPEndPoint ep = null;
-                                                                            var receiveAsync = Observable.FromAsyncPattern(
-                                                                                client.BeginReceive,
-                                                                                ar => client.EndReceive(ar, ref ep));
+                            // Set up the observable for receiveAsync.
+                            IPEndPoint ep = null;
+                            var receiveAsync = Observable.FromAsyncPattern(
+                                client.BeginReceive,
+                                ar => client.EndReceive(ar, ref ep));
 
-                                                                            return Observable.Defer(receiveAsync)
-                                                                                .Repeat()
-                                                                                .Select(bytes => Tuple.Create(ep, bytes));
-                                                                        }));
+                            // Combine Defer and Repeat -> repeatedly call receiveAsync()
+                            return Observable.Defer(receiveAsync)
+                                .Select(bytes => Tuple.Create(ep, bytes))
+                                .Repeat();
+                        }));
 
             discovery.Subscribe(x => Console.WriteLine("{0}: {1}", x.Item1, Encoding.ASCII.GetString(x.Item2)));
+
+            // TODO: How to get Rx to just wait on the observable sequence on the current thread for the timeout?
             Thread.Sleep(5000);
         }
     }
