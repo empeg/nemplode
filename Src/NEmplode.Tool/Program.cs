88e8d8cceca7717f253ddabb3bc09e704c4cfce6
Roger Lipscombe <roger@differentpla.net>
2011-02-27 19:18:00 +0000

    Correctly dispose of UdpClient.
    * Added call to Observable.Using.
    
    See #1.

--- Src/NEmplode.Tool/Program.cs
+++ Src/NEmplode.Tool/Program.cs
@@ -15,23 +15,28 @@ namespace NEmplode.Tool
             // To find an empeg, we do a UDP broadcast on port 8300 with a single question mark in it.
             const int empegDiscoveryPort = 8300;
 
-            var discovery = Observable.Defer(() =>
-                                                 {
-                                                     byte[] requestBytes = new byte[] { 0x3F }; // Single '?'
+            var discovery = Observable.Defer(() => Observable.Using(() =>
+                                                                        {
+                                                                            var localEndPoint = new IPEndPoint(IPAddress.Any, empegDiscoveryPort);
+                                                                            UdpClient client = new UdpClient(localEndPoint);
+                                                                            client.EnableBroadcast = true;
+                                                                            return client;
+                                                                        },
+                                                                    client =>
+                                                                        {
+                                                                            byte[] requestBytes = new byte[] { 0x3F }; // Single '?'
+                                                                            var remoteEndPoint = new IPEndPoint(IPAddress.Broadcast, empegDiscoveryPort);
+                                                                            client.Send(requestBytes, requestBytes.Length, remoteEndPoint);
 
-                                                     var localEndPoint = new IPEndPoint(IPAddress.Any, empegDiscoveryPort);
-                                                     UdpClient client = new UdpClient(localEndPoint);
-                                                     client.EnableBroadcast = true;
+                                                                            IPEndPoint ep = null;
+                                                                            var receiveAsync = Observable.FromAsyncPattern(
+                                                                                client.BeginReceive,
+                                                                                ar => client.EndReceive(ar, ref ep));
 
-                                                     var remoteEndPoint = new IPEndPoint(IPAddress.Broadcast, empegDiscoveryPort);
-                                                     client.Send(requestBytes, requestBytes.Length, remoteEndPoint);
-
-                                                     IPEndPoint ep = null;
-                                                     var receiveAsync = Observable.FromAsyncPattern(client.BeginReceive,
-                                                                                                    ar => client.EndReceive(ar, ref ep));
-
-                                                     return Observable.Defer(receiveAsync).Repeat().Select(bytes => Tuple.Create(ep, bytes));
-                                                 });
+                                                                            return Observable.Defer(receiveAsync)
+                                                                                .Repeat()
+                                                                                .Select(bytes => Tuple.Create(ep, bytes));
+                                                                        }));
 
             discovery.Subscribe(x => Console.WriteLine("{0}: {1}", x.Item1, Encoding.ASCII.GetString(x.Item2)));
             Thread.Sleep(5000);
