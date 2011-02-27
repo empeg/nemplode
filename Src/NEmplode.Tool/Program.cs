e4a22d44c97589e593b7c29f7631a40497c8a674
Roger Lipscombe <roger@differentpla.net>
2011-02-27 16:28:39 +0000

    Empeg discovery
    See #1.

--- Src/NEmplode.Tool/Program.cs
+++ Src/NEmplode.Tool/Program.cs
@@ -24,90 +24,51 @@ namespace NEmplode.Tool
         [STAThread]
         static void Main(string[] args)
         {
-            // To find an empeg, we do a UDP broadcast on
-            // port 8300 with a single question mark in it.
-            IPEndPoint sourceEndPoint = new IPEndPoint(IPAddress.Any,
-                                                            8300);
-            IPEndPoint destinationEndPoint = new IPEndPoint(IPAddress.Broadcast,
-                                                            8300);
+            // To find an empeg, we do a UDP broadcast on port 8300 with a single question mark in it.
+            const int empegDiscoveryPort = 8300;
 
-            byte[] requestBytes = Encoding.ASCII.GetBytes("?");
+            byte[] requestBytes = new byte[] { 0x3F }; // Single '?'
 
-            try
+            using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp))
             {
-                Socket s = new Socket(sourceEndPoint.AddressFamily,
-                                      SocketType.Dgram, ProtocolType.Udp);
-                s.Bind(sourceEndPoint);
-                s.SetSocketOption(SocketOptionLevel.Socket,
-                                      SocketOptionName.Broadcast, 1);
+                var localEndPoint = new IPEndPoint(IPAddress.Any, empegDiscoveryPort);
+                socket.Bind(localEndPoint);
 
-                Console.WriteLine("Looking for empegs on {0}...",
-                                      destinationEndPoint);
-                s.SendTo(requestBytes, destinationEndPoint);
+                socket.SetSocketOption(SocketOptionLevel.Socket,
+                                  SocketOptionName.Broadcast, 1);
 
-                EndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+                var remoteEndPoint = new IPEndPoint(IPAddress.Broadcast, empegDiscoveryPort);
+                Console.WriteLine("Looking for empegs on {0}...", remoteEndPoint);
+                socket.SendTo(requestBytes, remoteEndPoint);
 
-                StateObject so = new StateObject(s);
-                s.BeginReceiveFrom(so.ResponseBytes, 0,
-                                      StateObject.ResponseBufferSize, 0,
-                                      ref remoteEndPoint,
-                                      ResponseCallback, so);
+                StateObject so = new StateObject(socket);
+                EndPoint ep = new IPEndPoint(0, 0);
+                socket.BeginReceiveFrom(so.ResponseBytes, 0,
+                                   StateObject.ResponseBufferSize, 0,
+                                   ref ep,
+                                   ResponseCallback, so);
 
                 // Then wait for 5s.
                 Thread.Sleep(5000);
             }
-            catch (SocketException e)
-            {
-                Console.WriteLine("SocketException: {0} ({1})",
-                                      e, e.ErrorCode);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Exception: {0}",
-                                      e);
-            }
-
         }
 
         public static void ResponseCallback(IAsyncResult ar)
         {
-            try
-            {
-                StateObject so = (StateObject)ar.AsyncState;
-                Socket client = so.Socket;
+            StateObject so = (StateObject)ar.AsyncState;
+            Socket client = so.Socket;
 
-                EndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
-                int responseBytesReceived = client.EndReceiveFrom(ar,
-                                      ref remoteEndPoint);
+            EndPoint ep = new IPEndPoint(IPAddress.Any, 0);
+            int responseBytesReceived = client.EndReceiveFrom(ar, ref ep);
 
-                Console.WriteLine("Received response from {0}",
-                                      remoteEndPoint);
+            Console.WriteLine("Received response from {0}", ep);
 
-                // The received data is in the buffer originally passed.
-                // So, we can either hand it over in the state object and
-                // look at it here; or we can set an event and
-                // release the other thread to look at it.
-                // We'll do it in the response object, because
-                // it makes it a little easier.
-                string responseString = Encoding.ASCII.GetString(so.ResponseBytes,
-                                            0, responseBytesReceived);
+            string responseString = Encoding.ASCII.GetString(so.ResponseBytes, 0, responseBytesReceived);
 
-                Console.WriteLine("Response contains: {0}",
-                                            responseString);
+            Console.WriteLine("Response contains: {0}", responseString);
 
-
-                // Issue another BeginReceiveFrom
-                client.BeginReceiveFrom(so.ResponseBytes, 0,
-                                            StateObject.ResponseBufferSize, 0,
-                                            ref remoteEndPoint,
-                                            ResponseCallback,
-                                            so);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Exception in ResponseCallback: {0}",
-                                      e);
-            }
+            // Issue another BeginReceiveFrom
+            client.BeginReceiveFrom(so.ResponseBytes, 0, StateObject.ResponseBufferSize, 0, ref ep, ResponseCallback, so);
         }
     }
 }
